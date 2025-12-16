c6bc1ed63dd0e09700333ed874e133b3d26664b6
hikaribrandan3-code <hikaribrandan3@gmail.com>
2025-12-16 01:51:59 -0300

    Font sizes: Home 30px, Menu/Info/Pedido 25px

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -16,7 +16,7 @@ function PageHeader({ businessName }) {
             marginBottom: 0
         }}>
             <span style={{
-                fontSize: 24,
+                fontSize: 25,
                 fontWeight: 600,
                 color: '#1F2937',
                 letterSpacing: '-0.01em',
--- src/pages/customer/Home.jsx
+++ src/pages/customer/Home.jsx
@@ -118,7 +118,7 @@ function Home() {
                 textAlign: 'center'
             }}>
                 <h1 style={{
-                    fontSize: 28,
+                    fontSize: 30,
                     fontWeight: 600,
                     color: '#4A4238',
                     margin: 0,
