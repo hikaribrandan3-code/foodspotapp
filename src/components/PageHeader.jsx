89ad2cb673a6071dd527ce9a94adfbec653c4bd3
hikaribrandan3-code <hikaribrandan3@gmail.com>
2025-12-16 01:44:41 -0300

    PageHeader: increase font size to 23px

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -16,7 +16,7 @@ function PageHeader({ businessName }) {
             marginBottom: 0
         }}>
             <span style={{
-                fontSize: 21,
+                fontSize: 23,
                 fontWeight: 600,
                 color: '#1F2937',
                 letterSpacing: '-0.01em',
